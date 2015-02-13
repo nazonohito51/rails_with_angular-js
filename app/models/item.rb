d0e359d87b2112e6d9a915833e99362dbc29b626
S.Kawashima <s.kawashima@hachimenroppi.com>
2015-02-13 15:48:04 +0900

    edit validate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ActiveRecord::Base
-  validate :name, :presence => true, :length => { :maximum => 255 }
-  validate :price, :presence => true, :numericality => { :greater_than => 0 }
+  validates :name, :presence => true, :length => { :maximum => 255 }
+  validates :price, :presence => true, :numericality => { :greater_than => 0 }
 end
