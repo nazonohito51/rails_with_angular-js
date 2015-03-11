4b510b0a788c7d84879547e6f6d49f29db158c08
nazonohito51 <nazonohito51@hotmail.com>
2015-03-12 00:09:38 +0900

    add karma

---
+++ spec/javascript/angular/first_testSpec.js
@@ -0,0 +1,13 @@
+describe('add 関数のテスト', function() {
+    it('1 + 1 は 2', function() {
+        expect(1 + 1).toBe(2);
+    });
+
+    it('1 + 4 は 5', function() {
+        expect(1 + 4).toBe(5);
+    });
+
+    it('10 + 2 は 12', function() {
+        expect(10 + 2).toBe(5);
+    });
+});
